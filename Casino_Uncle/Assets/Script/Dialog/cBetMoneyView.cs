8f0aedea42e9c1e5228875655e9ce815dd711602
Hideya Sato <satouh@alfasystem.co.jp>
2016-06-17 15:32:04 +0900

    ベット関連を修正

---
+++ Casino_Uncle/Assets/Script/Dialog/cBetMoneyView.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class cBetMoneyView : MonoBehaviour {
+
+	public cBetMoneyModel m_betMoneyModel;
+	public InputField m_Input;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		m_Input.interactable = m_betMoneyModel.GetIput();
+	}
+}
