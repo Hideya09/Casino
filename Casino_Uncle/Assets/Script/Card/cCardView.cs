a669b8a7e38e6a786828cfaed64721bc0d293273
Hideya Sato <satouh@alfasystem.co.jp>
2016-06-23 14:46:18 +0900

    カードの描画方法を修正

--- Casino_Uncle/Assets/Script/Card/cCardView.cs
+++ Casino_Uncle/Assets/Script/Card/cCardView.cs
@@ -34,29 +34,7 @@ public class cCardView : MonoBehaviour {
 		transform.localPosition = m_cModel.GetPosition ();
 		transform.rotation = Quaternion.Euler (m_cModel.GetRotation ());
 
-		//描画モードに応じた変更
-		if (m_cModel.m_DrawMode == cCardModel.eDrawMode.eDrawMode_Back) {
-			m_Number = m_Back;
-			m_Renderer.sprite = m_Sprite.sprite [m_Number];
-		} else if (m_Number != m_cModel.m_CardNumber) {
-			m_Number = m_cModel.m_CardNumber;
-			m_Renderer.sprite = m_Sprite.sprite [m_Number];
-		}
-
-		if (m_cModel.m_DrawMode == cCardModel.eDrawMode.eDrawMode_None) {
-			m_Renderer.enabled = false;
-		} else {
-			m_Renderer.enabled = true;
-		}
-
-		if (m_cModel.m_DrawMode == cCardModel.eDrawMode.eDrawMode_Dark) {
-			//Color color = m_Renderer.color;
-			m_Renderer.color = new Color (0.5f, 0.5f, 0.5f, m_cModel.GetFade ());
-
-		} else {
-			//Color color = m_Renderer.color;
-			m_Renderer.color = new Color (1.0f, 1.0f, 1.0f, m_cModel.GetFade ());
-		}
+		float fade = m_cModel.GetFade ();
 
 		//サイズの変更
 		switch (m_cModel.m_Size) {
@@ -78,19 +56,47 @@ public class cCardView : MonoBehaviour {
 		switch (m_cModel.m_OutLineMode) {
 		case cCardModel.eOutLineMode.eOutLineMode_None:
 			m_OutLine.enabled = false;
+			m_Renderer.color = new Color (1.0f, 1.0f, 1.0f, fade);
 			break;
 		case cCardModel.eOutLineMode.eOutLineMode_Yellow:
 			m_OutLine.enabled = true;
-			m_OutLine.effectColor = new Color (1.0f, 1.0f, 0.0f, 1.0f);
+			m_OutLine.effectColor = new Color (1.0f, 1.0f, 1.0f - fade, fade);
+			m_Renderer.color = new Color (1.0f - ((1.0f - fade)), 1.0f - ((1.0f - fade)), 1.0f, fade);
 			break;
 		case cCardModel.eOutLineMode.eOutLineMode_Blue:
 			m_OutLine.enabled = true;
-			m_OutLine.effectColor = new Color (0.0f, 0.0f, 1.0f, 1.0f);
+			m_OutLine.effectColor = new Color (1.0f - fade, 1.0f - fade, 1.0f, m_cModel.GetFade ());
+			m_Renderer.color = new Color (1.0f, 1.0f, 1.0f - ((1.0f - fade)), fade);
 			break;
 		case cCardModel.eOutLineMode.eOutLineMode_Red:
 			m_OutLine.enabled = true;
-			m_OutLine.effectColor = new Color (1.0f, 0.0f, 0.0f, 1.0f);
+			m_OutLine.effectColor = new Color (1.0f, 1.0f - fade, 1.0f - fade, m_cModel.GetFade ());
+			m_Renderer.color = new Color (1.0f - ((1.0f - fade)), 1.0f, 1.0f, fade);
 			break;
+		}
+
+		//描画モードに応じた変更
+		if (m_cModel.m_DrawMode == cCardModel.eDrawMode.eDrawMode_Back) {
+			m_Number = m_Back;
+			m_Renderer.sprite = m_Sprite.sprite [m_Number];
+		} else if (m_Number != m_cModel.m_CardNumber) {
+			m_Number = m_cModel.m_CardNumber;
+			m_Renderer.sprite = m_Sprite.sprite [m_Number];
+		}
+
+		if (m_cModel.m_DrawMode == cCardModel.eDrawMode.eDrawMode_None) {
+			m_Renderer.enabled = false;
+		} else {
+			m_Renderer.enabled = true;
+		}
+
+		if (m_cModel.m_DrawMode == cCardModel.eDrawMode.eDrawMode_Dark) {
+			//Color color = m_Renderer.color;
+			m_Renderer.color *= new Color (0.5f, 0.5f, 0.5f, 1.0f);
+
+		} else {
+			//Color color = m_Renderer.color;
+			m_Renderer.color *= new Color (1.0f, 1.0f, 1.0f, 1.0f);
 		}
 	}
 }
